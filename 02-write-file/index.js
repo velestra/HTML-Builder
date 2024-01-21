6dcfbbef3048d3ab11ba0666754a6aa7abea490d
velestra <150198911+velestra@users.noreply.github.com>
2024-01-21 14:50:30 -0600

    feat: solve second task

---
+++ 02-write-file/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const readline = require('readline');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'output.txt');
+const writeFile = fs.createWriteStream(filePath, {encoding: 'utf-8', flags: 'a'});
+
+console.log('Welcome! Input your text. For exit input "exit"');
+
+const rl = readline.createInterface ({
+  input: process.stdin,
+  output: process.stdout
+});
+function manageInput(input) {
+  if(input.trim().toLowerCase() === 'exit') {
+    process.exit();
+  }
+  writeFile.write(`${input}\n`);
+  rl.question('', manageInput);
+}
+
+rl.question('', manageInput);
+
+process.on('SIGINT', () => {
+  process.exit();
+});
